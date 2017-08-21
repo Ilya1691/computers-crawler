74a2db7500df66c9c635defb30d07a24642023c1
Ilya Rustaveli <irustaveli@gmail.com>
2017-08-21 18:35:01 +0300

    bench

--- parsers/product/product_parser.py
+++ parsers/product/product_parser.py
@@ -14,8 +14,8 @@ class ProductParser:
         product_ch = {}
 
         for item in product_items:
-            name = item.find('div', {'class': 'group_prop'}).text
-            value = item.find('div', {'class': 'prop_name'}).text
+            name = item.find('div', {'class': 'prop_name st2'}).text
+            value = item.find('div', {'class': 'st2'}).text
 
             if name in self.product_characteristics:
                 param_name = self.product_characteristics[name]
